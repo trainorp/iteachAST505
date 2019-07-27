68137a536f424f1e05918f0edbbd4c9bab42f02b
trainorp <patrick.trainor@louisville.edu>
2019-07-27 08:38:05 -0600

    working

--- myLectures/lecture3_DescriptiveStatistics/Lecture3.R
+++ myLectures/lecture3_DescriptiveStatistics/Lecture3.R
@@ -77,3 +77,55 @@ ggplot(stagec, aes(x = g2, y = 6*..density..)) +
   geom_histogram(breaks = seq(2, 56, 6), col = "black", fill = "grey65") + 
   scale_x_continuous("Percentage of cells in G2 phase", seq(2, 56, 6)) + theme_bw() + labs(y = "Relative Frequency")
 dev.off()
+
+########### Prostate cancer dataset ############
+df1 <- data.frame(x1 = rnorm(100000, 67, 5), x2 = c(rnorm(50000, 60, 2.5),rnorm(50000, 70, 2.5)), 
+                  Sex = factor(c(rep("Female", 50000), rep("Male", 50000))))
+
+# Unimodal:
+png(filename = "histNormal.png", height = 4, width = 6, units = "in", res = 600)
+ggplot(df1, aes(x = x1)) + geom_histogram(bins = 30, color = "black", fill = "grey65") + 
+  theme_bw() + labs(x = "Height (Inches)", y = "Frequency") +
+  ggtitle("Data distribution of the height of some people") + theme(plot.title = element_text(hjust = 0.5))
+dev.off()
+
+# Bimodal:
+png(filename = "histNormal2.png", height = 4, width = 6, units = "in", res = 600)
+ggplot(df1, aes(x = x2)) + geom_histogram(bins = 30, color = "black", fill = "grey65") + 
+  theme_bw() + labs(x = "Height (Inches)", y = "Frequency") +
+  ggtitle("Data distribution of the height of some people") + theme(plot.title = element_text(hjust = 0.5))
+dev.off()
+
+# Bimodal showing group
+png(filename = "histNormal2b.png", height = 4, width = 6.5, units = "in", res = 600)
+ggplot(df1, aes(x = x2, fill = Sex)) + geom_histogram(bins = 30, color = "black", alpha = .5, position = "identity") + 
+  theme_bw() + labs(x = "Height (Inches)", y = "Frequency") +
+  ggtitle("Data distribution of the height of some people") + theme(plot.title = element_text(hjust = 0.5))
+dev.off()
+
+# Symmetric:
+png(filename = "histNormalSym.png", height = 4, width = 6, units = "in", res = 600)
+ggplot(df1, aes(x = x1)) + geom_histogram(bins = 30, color = "black", fill = "grey65") + 
+  geom_vline(xintercept = 67, color = "darkblue", lwd = 2) +
+  theme_bw() + labs(x = "Height (Inches)", y = "Frequency") +
+  ggtitle("Data distribution of the height of some people") + theme(plot.title = element_text(hjust = 0.5))
+dev.off()
+
+# Generate some beta RV's for showing right- and left-tailed distributions:
+betaMode <- function(alpha, beta) (alpha - 1)/(alpha + beta - 2)
+df2 <- data.frame(x1 = rbeta(100000, 3, 11))
+df2$x2 <- 1 - df2$x1
+
+png(file = "histRightSkew.png", height = 4, width = 6, units = "in", res = 600)
+ggplot(df2, aes(x = x1)) + geom_histogram(bins = 30, color = "black", fill = "grey65") + 
+  geom_vline(xintercept = betaMode(3, 12), color = "darkblue", lwd = 2) + theme_bw() + 
+  labs(x = "Weight (kg)", y = "Frequency") + ggtitle("Weight of some fish") + 
+  theme(plot.title = element_text(hjust = 0.5))
+dev.off()
+
+png(file = "histLeftSkew.png", height = 4, width = 6, units = "in", res = 600)
+ggplot(df2, aes(x = x2)) + geom_histogram(bins = 30, color = "black", fill = "grey65") + 
+  geom_vline(xintercept = 1 - betaMode(3, 11), color = "darkblue", lwd = 2) + theme_bw() +
+  labs(x = "Weight (kg)", y = "Frequency") + ggtitle("Weight of some other fish") + 
+  theme(plot.title = element_text(hjust = 0.5))
+dev.off()

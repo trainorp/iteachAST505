875e4d27e4787fd5f6f644af5dfdb00a6ee8c20a
trainorp <patrick.trainor@louisville.edu>
2019-08-08 17:17:30 -0600

    working

---
+++ myLectures/lecture6_RandomVariablesProbDist/lecture6.R
@@ -0,0 +1,10 @@
+############ Prereqs ############
+options(stringsAsFactors = FALSE, scipen = 600)
+oldPar <- par()
+os <- Sys.info()["sysname"]
+baseDir <- ifelse(os == "Windows", "C:/Users/ptrainor/gdrive", "~/gdrive")
+
+library(tidyverse)
+library(ggrepel)
+
+setwd(paste0(baseDir, "/iTeach/AST_505/myLectures/lecture6_RandomVariableProbDist"))

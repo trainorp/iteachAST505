66ddd027744399b53b2affe304385759df0fbd32
trainorp <patrick.trainor@louisville.edu>
2019-09-02 13:49:33 -0600

    working

--- myLectures/lecture11_Nonparametric/lecture11.R
+++ myLectures/lecture11_Nonparametric/lecture11.R
@@ -40,6 +40,12 @@ set.seed(333)
 x2 <- round((rweibull(6, shape = 1.5, scale = 3) + 2.5) * 12,1)
 df3 <- data.frame(time = c(x1, x2), stage = c(rep("Stage 4", times = 6), rep("Stage 3", times = 6)))
 
+# Boxplot:
+png(file = "boxplot.png", height = 4, width = 6, units = "in", res = 600)
+set.seed(333)
+ggplot(df3, aes(x = stage, y = time, fill = stage)) + geom_boxplot() + geom_jitter(width = .05) + theme_bw()
+dev.off()
+
 # Tests:
 wilcox.test(x = x1, y = x2)
 test1 <- wilcox.test(time ~ stage, data = df3, exact = TRUE, alternative = "greater", conf.int = TRUE)
@@ -59,3 +65,24 @@ combList$diff <- combList$x1Val - combList$x2Val
 sort(combList$diff)
 
 wilcox.test(time ~ stage, data = df3, exact = TRUE, alternative = "two.sided", conf.int = TRUE)
+
+
+########### Simulated data for normal appoximation ############
+# micrograms per liter as phycocyanin 
+set.seed(2)
+x1 <- round(exp(rnorm(n = 100, mean = log(222), sd = 1/2)), 1)
+set.seed(22)
+x2 <- round(exp(rnorm(n = 100, mean = log(300), sd = 1/2)), 1)
+df4 <- data.frame(value = c(x1, x2), month = c(rep("June", 100), rep("August", 100)))
+df4$month <- factor(df4$month, levels = c("June", "August"))
+
+png(file = "RoughHist.png", height = 5, width = 6.5, units = "in", res = 600)
+ggplot(df4, aes(x = value, y = ..density..)) + geom_histogram(color = "black", fill = "grey70") + 
+  facet_wrap(~month, nrow = 2) + theme_bw() + labs(x = expression(paste(mu, "g per liter")), y = "Density")
+dev.off()
+
+df4 <- df4 %>% arrange(value)
+df4Counts <- df4 %>% group_by(value) %>% summarize(n = n())
+df4 <- df4 %>% left_join(df4Counts)
+df4$ranks <- rank(df4$value, ties = "average")
+sum(df4$ranks[df4$month == "June"])

d58d98364d2495b45fa586ebfd35148864f0c4d0
trainorp <patrick.trainor@louisville.edu>
2019-09-04 09:12:54 -0600

    working

--- myLectures/lecture11_Nonparametric/lecture11.R
+++ myLectures/lecture11_Nonparametric/lecture11.R
@@ -85,4 +85,61 @@ df4 <- df4 %>% arrange(value)
 df4Counts <- df4 %>% group_by(value) %>% summarize(n = n())
 df4 <- df4 %>% left_join(df4Counts)
 df4$ranks <- rank(df4$value, ties = "average")
+
+# Test statistic:
 sum(df4$ranks[df4$month == "June"])
+
+# Ties:
+df4 %>% filter(n >1)
+
+# Test:
+wilcox.test(value ~ month, data = df4)
+wilcox.test(x = df4$value[df4$month == "August"], y = df4$value[df4$month == "June"])
+
+test2 <- wilcox.test(x = df4$value[df4$month == "June"], y = df4$value[df4$month == "August"])
+
+test2$statistic + 100 * (100 + 1) / 2
+
+wilcox.test(x = df4$value[df4$month == "June"], y = df4$value[df4$month == "August"], correct = FALSE)
+
+# Point estimate:
+combList <- expand.grid(x1 = 1:100, x2 = 1:100)
+combList$diff <- combList$x2Val <- combList$x1Val <- NA
+for(i in 1:nrow(combList)){
+  combList$x1Val[i] <- x1[combList$x1[i]]
+  combList$x2Val[i] <- x2[combList$x2[i]]
+}
+combList$diff <- combList$x1Val - combList$x2Val
+combList$ranks <- rank(combList$diff, ties = "average")
+
+wilcox.test(x = df4$value[df4$month == "June"], y = df4$value[df4$month == "August"], conf.int = TRUE, 
+            conf.level = .9, correct = FALSE)
+
+############ Swimming example ############
+set.seed(333)
+df5 <- data.frame(baseline = exp(rnorm(n = 10, mean = log(2), sd = .1)) * 60)
+
+set.seed(3333)
+df5$post <- exp(log(df5$baseline) + rnorm(10, mean = -0.025, sd = .03))
+df5$diff <- df5$post - df5$baseline
+df5$id <- 1:nrow(df5)
+df6 <- df5 %>% gather(key = "timepoint", value = "time", -id)
+df6$id <- factor(df6$id)
+
+png(file = "scatter.png" , height = 4, width = 5, units = "in", res = 600)
+ggplot(df5, aes(x = baseline, y = post)) + geom_point() + theme_bw() +
+  labs(x = "Baseline time (seconds)", y = "Post-training (seconds)") +
+  ggtitle("Baseline versus post-training times") + theme(plot.title = element_text(hjust = 0.5))
+dev.off()
+
+png(file = "timecourse.png", height = 4, width = 6, units = "in", res = 600)
+ggplot(df6 %>% filter(timepoint != "diff"), aes(x = timepoint, y = time, color = id)) + geom_point() + 
+  geom_line(aes(group = id)) + theme_bw() + labs(x = "Time-point", y = "200M Time (seconds)", color = "Athlete") +
+  ggtitle("Baseline versus post-training times") + theme(plot.title = element_text(hjust = 0.5))
+dev.off()
+
+df5 <- df5 %>% arrange(abs(diff))
+df5$rank <- 1:nrow(df5)
+round(df5[, names(df5) %in% c("diff", "rank")], 1)
+wilcox.test(x = df5$baseline, y = df5$post, paired = TRUE)
+wilcox.test(y = df5$baseline, x = df5$post, paired = TRUE)
